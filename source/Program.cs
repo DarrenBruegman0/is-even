2309954b2ce2c8d3ed5af6693a2a4315fc34f448
DarrenBruegman0 <db1027061@otc.edu>
2025-09-25 08:59:04 -0500

    Update Program.cs
    added code comments

--- source/Program.cs
+++ source/Program.cs
@@ -1,8 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 while (true)
 {
+    //input number
     Console.Write("input a number>  ");
     int even_or_odd = Convert.ToInt32(Console.ReadLine());
+    //determines evenness and outputs
     if (even_or_odd % 2 == 0)
     {
         Console.WriteLine($"The Number {even_or_odd} is Even!");
